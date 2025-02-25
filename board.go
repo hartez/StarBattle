15d9ab4eeb09fdd7dd954983ac0ce6a30a4c7767
E.Z. Hart <hartez@gmail.com>
2025-02-25 12:33:01 -0700

    Output functions

--- board.go
+++ board.go
@@ -71,6 +71,60 @@ func Parse(fileName string) Board {
 	return board
 }
 
+func (board Board) String() string {
+
+	var sb strings.Builder
+
+	size := board.size
+
+	plural := "s"
+	if board.stars == 1 {
+		plural = ""
+	}
+
+	sb.WriteString(fmt.Sprintf("%d x %d, %d star%s\n", size, size, board.stars, plural))
+
+	for row := range size {
+		for col := range size {
+
+			value, err := board.value(row, col)
+
+			if err != nil {
+				return err.Error()
+			}
+
+			region, err := board.region(row, col)
+
+			if err != nil {
+				return err.Error()
+			}
+
+			sb.WriteString(sectionColor(region))
+			sb.WriteString("[")
+			sb.WriteString(value.String())
+			sb.WriteString("]")
+			sb.WriteString("\033[0m")
+		}
+
+		sb.WriteString("\n")
+	}
+
+	return sb.String()
+}
+
+func sectionColor(section int) string {
+
+	modification := 0
+
+	if section > 7 {
+		modification = 1 // bold
+	} else if section > 14 {
+		modification = 4 // underline
+	}
+
+	return fmt.Sprintf("\033[%d;%dm", modification, ((section % 7) + 31))
+}
+
 func (board Board) index(row int, col int) (int, error) {
 
 	boardSize := board.size
@@ -81,6 +135,27 @@ func (board Board) index(row int, col int) (int, error) {
 	}
 
 	return boardSize*row + col, nil
+}
+
+func (board Board) value(row int, column int) (Square, error) {
+	index, err := board.index(row, column)
+
+	if err != nil {
+		return -1, err
+	}
+
+	return board.squares[index], nil
+}
+
+func (board Board) setValue(row int, column int, value Square) error {
+	index, err := board.index(row, column)
+
+	if err != nil {
+		return err
+	}
+
+	board.squares[index] = value
+	return nil
 }
 
 func (board Board) region(row int, col int) (int, error) {
--- main.go
+++ main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Printf("Hello")
+	puzzleFile := "5_1.txt"
+
+	if len(os.Args) > 1 {
+		puzzleFile = os.Args[1]
+	}
+
+	fmt.Printf("Working on %s\n", puzzleFile)
+
+	board := Parse(puzzleFile)
+
+	fmt.Print(board)
 }
