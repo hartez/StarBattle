513d6f8d1195c181fd4a266e3d99ff3261c41818
E.Z. Hart <hartez@gmail.com>
2025-03-15 18:40:46 -0600

    Naive parallel solving

--- board.go
+++ board.go
@@ -442,3 +442,34 @@ func (board Board) eliminateAdjacentSquares(row int, col int) {
 		}
 	}
 }
+
+func (board Board) SolveParallel(solution chan Board) {
+
+	if !board.isValid() {
+		return
+	}
+
+	if board.hasEnoughStars() {
+		solution <- board
+		return
+	}
+
+	nextRow, nextCol, err := board.findEmptySquare()
+
+	if err != nil {
+		return
+	}
+
+	nextBoard := board.copy()
+
+	nextBoard.setValue(nextRow, nextCol, STAR)
+
+	nextBoard.eliminateSquares(nextRow, nextCol)
+
+	go nextBoard.SolveParallel(solution)
+
+	nextBoard = board.copy()
+	nextBoard.setValue(nextRow, nextCol, NOTSTAR)
+
+	go nextBoard.SolveParallel(solution)
+}
--- main.go
+++ main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
+	"runtime"
 	"time"
 )
 
 func main() {
-	puzzleFile := "5_1.txt"
+	parallel := flag.Bool("p", false, "solve puzzle in parallel")
+	puzzleFile := flag.String("i", "5_1.txt", "puzzle input file")
 
-	if len(os.Args) > 1 {
-		puzzleFile = os.Args[1]
+	flag.Parse()
+
+	if *parallel {
+		SolveInParallel(*puzzleFile)
+	} else {
+		SolveSequential(*puzzleFile)
 	}
+}
 
+func SolveSequential(puzzleFile string) {
 	board := Parse(puzzleFile)
 
 	fmt.Printf("Working on %s\n", puzzleFile)
@@ -30,6 +38,32 @@ func main() {
 	} else {
 		fmt.Printf("Puzzle could not be solved.\n")
 	}
+
+	fmt.Printf("Solve time was %s\n", end.Sub(start))
+}
+
+func SolveInParallel(puzzleFile string) {
+	board := Parse(puzzleFile)
+
+	solutionChannel := make(chan Board)
+
+	maxProcs := runtime.GOMAXPROCS(0)
+	numCPU := runtime.NumCPU()
+
+	fmt.Printf("CPUs: %d; Max processes: %d\n", numCPU, maxProcs)
+	fmt.Printf("Working on %s\n", puzzleFile)
+	fmt.Print(board)
+
+	start := time.Now()
+
+	go board.SolveParallel(solutionChannel)
+
+	solution := <-solutionChannel
+
+	end := time.Now()
+
+	fmt.Printf("Solved!\n")
+	fmt.Print(solution)
 
 	fmt.Printf("Solve time was %s\n", end.Sub(start))
 }
